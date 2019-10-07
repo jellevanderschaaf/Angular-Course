08f229897ee7f39643f41f3f4e29157540ec7921
jellevanderschaaf <50097870+jellevanderschaaf@users.noreply.github.com>
2019-10-07 22:45:45 +0200

    85

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Shopping-List-Recipy-Book';
+
+onNavigate(feature: string) {
+  this.loadedFeature = feature;
+}
+
 }
